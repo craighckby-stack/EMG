3242e1a4fbfdfb7f5ab3d76ddedfd1c7f09f0d6d
Craig Huckerby <craighckby@gmail.com>
2026-09-10 17:20:53 +1000

    EMG Core: Refactoring on src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-const rootElement: HTMLElement | null = document.getElementById('root');
+const rootElement = document.getElementById('root');
 
 if (!rootElement) {
   throw new Error('Failed to find the root element to mount the application.');
